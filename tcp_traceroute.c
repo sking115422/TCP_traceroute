2fd48b939b398a790b8e1a3c20a457d1e022cc72
student <spencerd.king@gmail.com>
2021-11-02 23:45:59 +0000

    fix connection

--- tcp_traceroute.c
+++ tcp_traceroute.c
@@ -27,7 +27,6 @@ int checkStringIsNumeric (char *str)
         }
     }
 
-
     if (count == strlen(str))
         return 1;
     else 
@@ -35,49 +34,101 @@ int checkStringIsNumeric (char *str)
 }
 
 
-int openConnection(char *TARGET, int portnum)
+// int openConnection(char *TARGET, int portnum)
+// {
+//     struct hostent *he;
+//     struct in_addr **addr_list;
+//     struct in_addr addr;
+
+//     char target_copy [strlen(TARGET)];
+
+//     strcpy(target_copy, TARGET);
+
+//     char * first_str = strtok(target_copy, ".");
+
+//     printf("target: %s\n", TARGET);
+
+//     char *first_ip;
+//     if (checkStringIsNumeric (first_str) == 0)
+//     {
+//         he = gethostbyname(TARGET);
+//         if (he == NULL) { 
+//             herror("gethostbyname"); 
+//             exit(0);
+//         }
+
+//         printf("\nDNS INFO\n");
+//         printf("Official name is: %s\n", he->h_name);
+//         first_ip = inet_ntoa(*(struct in_addr*)he->h_addr);
+//         printf("IP address: %s\n", first_ip);
+//         // printf("All addresses: ");
+//         // addr_list = (struct in_addr **)he->h_addr_list;
+//         // for(int i = 0; addr_list[i] != NULL; i++) {
+//         //     printf("%s ", inet_ntoa(*addr_list[i]));
+//         // }
+//         printf("\n");
+
+//     }
+//     else 
+//     {
+//         first_ip = TARGET;
+//     }
+
+//     printf("first_ip_1: %s\n", first_ip);
+
+
+//     int tcp_socket;
+
+//     //creating socket
+//     tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+//     struct sockaddr_in remote_address;
+
+//     remote_address.sin_family = AF_INET;
+//     remote_address.sin_port = htons(portnum);
+//     printf("first_ip_2: %s\n", first_ip);
+//     inet_aton(first_ip, &remote_address.sin_addr);
+
+    
+
+//     //establishing TCP connection
+//     if ( connect(tcp_socket, (struct sockaddr *) &remote_address, sizeof(remote_address)) != 0 )
+//     {
+//         close(tcp_socket);
+//         perror("Error");
+//         exit(0);
+//     }
+
+//     printf("\nNew raw TCP socket created!\n");
+
+//     return tcp_socket;
+// }
+
+
+int openConnection(const char *hostname, int portnum)
 {
     int i;
     struct hostent *he;
     struct in_addr **addr_list;
     struct in_addr addr;
 
-    char target_copy [strlen(TARGET)];
-
-    strcpy(target_copy, TARGET);
-
-    char * first_str = strtok(target_copy, ".");
-
-    printf("target: %s\n", TARGET);
-
-    char *first_ip;
-    if (checkStringIsNumeric (first_str) == 0)
-    {
-        he = gethostbyname(TARGET);
-        if (he == NULL) { 
-            herror("gethostbyname"); 
-            exit(0);
-        }
-
-        printf("\nDNS INFO\n");
-        printf("Official name is: %s\n", he->h_name);
-        first_ip = inet_ntoa(*(struct in_addr*)he->h_addr);
-        printf("IP address: %s\n", first_ip);
-        printf("All addresses: ");
-        addr_list = (struct in_addr **)he->h_addr_list;
-        for(i = 0; addr_list[i] != NULL; i++) {
-            printf("%s ", inet_ntoa(*addr_list[i]));
-        }
-        printf("\n");
-
-    }
-    else 
-    {
-        first_ip = TARGET;
+    //resolving hostname to IP
+    he = gethostbyname(hostname);
+    if (he == NULL) { 
+        herror("gethostbyname"); 
+        exit(0);
     }
 
-    printf("first_ip_1: %s\n", first_ip);
-
+    printf("\nDNS INFO\n");
+    printf("Official name is: %s\n", he->h_name);
+    char *first_ip = inet_ntoa(*(struct in_addr*)he->h_addr);
+    printf("IP address: %s\n", first_ip);
+    printf("All addresses: ");
+    addr_list = (struct in_addr **)he->h_addr_list;
+    for(i = 0; addr_list[i] != NULL; i++) {
+        printf("%s ", inet_ntoa(*addr_list[i]));
+    }
+    printf("\n");
 
     int tcp_socket;
 
@@ -88,7 +139,6 @@ int openConnection(char *TARGET, int portnum)
 
     remote_address.sin_family = AF_INET;
     remote_address.sin_port = htons(portnum);
-    printf("first_ip_2: %s\n", first_ip);
     inet_aton(first_ip, &remote_address.sin_addr);
 
     //establishing TCP connection
@@ -99,7 +149,7 @@ int openConnection(char *TARGET, int portnum)
         exit(0);
     }
 
-    printf("\nNew raw TCP socket created!\n");
+    printf("\nNew TCP socket created!\n");
 
     return tcp_socket;
 }
@@ -137,8 +187,14 @@ int main(int argc, char **argv)
     }
 
 
-    int conn = openConnection(TARGET, (int)*DST_PORT);
+    // int conn = openConnection(TARGET, (int)*DST_PORT);
 
+    int portnum = (int) *DST_PORT;          
+    char *hostname = TARGET;
+
+    int tcp_socket = openConnection(hostname, portnum);
+
+    printf("done...");
 }
 
 
