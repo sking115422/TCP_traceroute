ced53e04f0a6bf0ca5dcdb7867cf6c02ffcd88f2
student <spencerd.king@gmail.com>
2021-11-08 05:19:40 +0000

    program now sends raw packet to server

--- tcp_traceroute.c
+++ tcp_traceroute.c
@@ -4,6 +4,7 @@
 #include <unistd.h> 
 #include <string.h>
 #include <ctype.h>
+#include <signal.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -20,15 +21,12 @@
 int checkStringIsNumeric (char *str)
 {
     int count = 0;
-    printf("strlen: %ld\n", strlen(str));
     int i;
     for (i = 0;i< strlen(str); i++)
     {
-        printf("char: %c\n", str [i]);
         if (isdigit(str[i]) > 0)
         {
             count++;
-            printf("count: %d\n", count);
         }
     }
 
@@ -39,6 +37,74 @@ int checkStringIsNumeric (char *str)
 }
 
 
+char * resolveToIP(char *TARGET, int portnum)
+{
+    struct hostent *he;
+    struct in_addr **addr_list;
+    struct in_addr addr;
+
+    char target_copy [strlen(TARGET)];
+
+    strcpy(target_copy, TARGET);
+
+    char * first_str = strtok(target_copy, ".");
+
+    printf("target: %s\n", TARGET);
+
+    char *target_ip;
+    if (checkStringIsNumeric (first_str) == 0)
+    {
+        he = gethostbyname(TARGET);
+        if (he == NULL) 
+        { 
+            herror("gethostbyname"); 
+            exit(0);
+        }
+
+        printf("\nDNS INFO\n");
+        printf("Official name is: %s\n", he->h_name);
+        char *temp = inet_ntoa(*(struct in_addr*)he->h_addr);
+        strcpy(target_ip, temp);
+        printf("IP address: %s\n", target_ip);
+        printf("\n");
+    }
+    else 
+    {
+        target_ip = TARGET;
+    }
+
+    return target_ip;
+}
+
+unsigned short csum(unsigned short *ptr,int nbytes) 
+{
+	register long sum;
+	unsigned short oddbyte;
+	register short answer;
+
+	sum=0;
+	while(nbytes>1) {
+		sum+=*ptr++;
+		nbytes-=2;
+	}
+	if(nbytes==1) {
+		oddbyte=0;
+		*((u_char*)&oddbyte)=*(u_char*)ptr;
+		sum+=oddbyte;
+	}
+
+	sum = (sum>>16)+(sum & 0xffff);
+	sum = sum + (sum>>16);
+	answer=(short)~sum;
+	
+	return(answer);
+}
+
+
+void timeoutFunc (int signum)
+{
+    printf("Timeout occured! No ICMP packet received...\n");
+}
 
 
 int main(int argc, char **argv) 
@@ -72,79 +138,188 @@ int main(int argc, char **argv)
         }
     }
 
-    //Create a raw socket
-	int s = socket (PF_INET, SOCK_RAW, IPPROTO_TCP);
-	
-	if(s == -1)
-	{
-		//socket creation failed, may be because of non-root privileges
-		perror("Failed to create socket");
-		exit(1);
-	}
+    int portnum = atoi(DST_PORT);
+    char * target_ip = resolveToIP(TARGET, portnum);
+    int max_hops = atoi(MAX_HOPS);
 
-    //IP_HDRINCL to tell the kernel that headers are included in the packet
-	int one = 1;
-	const int *val = &one;
 
-    if (setsockopt (s, IPPROTO_IP, IP_HDRINCL, val, sizeof (one)) < 0)
-	{
-		perror("Error setting IP_HDRINCL");
-		exit(0);
-	}
-
-	//Datagram to represent the packet
-	char datagram[4096] , source_ip[32] , *data , *pseudogram;
-	
-	//zero out the packet buffer
-	memset (datagram, 0, 4096);
-	
-	//IP header
-	struct iphdr *iph = (struct iphdr *) datagram;
-	
-	//TCP header
-	struct tcphdr *tcph = (struct tcphdr *) (datagram + sizeof (struct ip));
-	struct sockaddr_in sin;
-	struct pseudo_header psh;
-	
-	//Data part
-	data = datagram + sizeof(struct iphdr) + sizeof(struct tcphdr);
-	strcpy(data , "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-	
-	//some address resolution
-	strcpy(source_ip , "192.168.1.2");
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons(80);
-	sin.sin_addr.s_addr = inet_addr ("1.2.3.4");
-	
-	//Fill in the IP Header
-	iph->ihl = 5;
-	iph->version = 4;
-	iph->tos = 0;
-	iph->tot_len = sizeof (struct iphdr) + sizeof (struct tcphdr) + strlen(data);
-	iph->id = htonl (54321);	//Id of this packet
-	iph->frag_off = 0;
-	iph->ttl = 255;
-	iph->protocol = IPPROTO_TCP;
-	iph->check = 0;		//Set to 0 before calculating checksum
-	iph->saddr = inet_addr ( source_ip );	//Spoof the source ip address
-	iph->daddr = sin.sin_addr.s_addr;
-
-    //Send the packet
-    if (sendto (s, datagram, iph->tot_len ,	0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
+    for (int i = 1; i < max_hops + 1; i++)
     {
-        perror("sendto failed");
-    }
-    //Data send successfully
-    else
-    {
-        printf ("Packet Send. Length : %d \n" , iph->tot_len);
-    }
-    // sleep for 1 seconds
-    sleep(1);
+
+        char dg [4096];
+
+        struct ip *iph = (struct ip *) dg;
+        struct tcphdr *tcph = (struct tcphdr *) dg + sizeof (struct ip);
+
+        struct sockaddr_in sain;
+
+        sain.sin_family = AF_INET;
+        sain.sin_port = htons(portnum);
+        sain.sin_addr.s_addr = inet_addr (target_ip);
+
+        iph->ip_hl = 5;
+        iph->ip_v = 4;
+        iph->ip_tos = 0;
+        iph->ip_len = sizeof (struct ip) + sizeof (struct tcphdr);	/* no payload */
+        iph->ip_id = htonl (54321);	/* the value doesn't matter here */
+        iph->ip_off = 0;
+        iph->ip_ttl = i;
+        iph->ip_p = 6;
+        iph->ip_sum = 0;		/* set it to 0 before computing the actual checksum later */
+        iph->ip_src.s_addr = inet_addr ("127.0.0.1");/* SYN's can be blindly spoofed */
+        iph->ip_dst.s_addr = sain.sin_addr.s_addr;
+        tcph->th_sport = htons (12345);	/* arbitrary port */
+        tcph->th_dport = htons (portnum);
+        tcph->th_seq = random ();/* in a SYN packet, the sequence is a random */
+        tcph->th_ack = 0;/* number, and the ack sequence is 0 in the 1st packet */
+        tcph->th_x2 = 0;
+        tcph->th_off = 0;		/* first and only tcp segment */
+        tcph->th_flags = TH_SYN;	/* initial connection request */
+        tcph->th_win = htonl (65535);	/* maximum allowed window size */
+        tcph->th_sum = 0;   /* if you set a checksum to zero, your kernel's IP stack should fill in the correct checksum during transmission */
+        tcph->th_urp = 0;
+        iph->ip_sum = csum ((unsigned short *) dg, iph->ip_len >> 1);
+
+        int sendsock = socket (PF_INET, SOCK_RAW, IPPROTO_TCP);
+
+        int one = 1;
+        const int *val = &one;
+
+        if (setsockopt (sendsock, IPPROTO_IP, IP_HDRINCL, val, sizeof (one)) < 0)
+        {
+            perror("Error setting IP_HDRINCL");
+            exit(0);
+        }
 
 
-    printf("done...");
+        if (sendto (sendsock, dg, iph->ip_len ,	0, (struct sockaddr *) &sain, sizeof (sain)) < 0)
+        {
+            perror("sendto failed");
+        }
+
+        else
+        {
+            printf ("Packet Sent. Length : %d \n" , iph->ip_len);
+        }
+
+        struct sockaddr saddr;
+        int saddr_size = sizeof(saddr);
+
+
+
+        int recvsock = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
+
+        unsigned char * buf = (unsigned char *) malloc(65536);
+        
+        int bytes_recieved = recvfrom(recvsock, buf, 65536, 0, &saddr, &saddr_size);
+
+        printf("bytes_recieved: %d", bytes_recieved);
+
+
+    }
+    
+
+
+
+    printf("done...\n");
+
+
+
 }
+
+
+////TEST 1
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // //Create a raw socket
+	// int s = socket (PF_INET, SOCK_RAW, IPPROTO_TCP);
+	
+	// if(s == -1)
+	// {
+	// 	//socket creation failed, may be because of non-root privileges
+	// 	perror("Failed to create socket");
+	// 	exit(1);
+	// }
+
+    // //IP_HDRINCL to tell the kernel that headers are included in the packet
+	// int one = 1;
+	// const int *val = &one;
+
+    // if (setsockopt (s, IPPROTO_IP, IP_HDRINCL, val, sizeof (one)) < 0)
+	// {
+	// 	perror("Error setting IP_HDRINCL");
+	// 	exit(0);
+	// }
+
+	// //Datagram to represent the packet
+	// char datagram[4096] , source_ip[32] , *data , *pseudogram;
+	
+	// //zero out the packet buffer
+	// memset (datagram, 0, 4096);
+	
+	// //IP header
+	// struct iphdr *iph = (struct iphdr *) datagram;
+	
+	// //TCP header
+	// struct tcphdr *tcph = (struct tcphdr *) (datagram + sizeof (struct ip));
+	// struct sockaddr_in sin;
+	
+	// //Data part
+	// data = datagram + sizeof(struct iphdr) + sizeof(struct tcphdr);
+	// strcpy(data , "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	
+	// //some address resolution
+	// strcpy(source_ip , "127.0.0.1");
+	// sin.sin_family = AF_INET;
+	// sin.sin_port = htons(80);
+	// sin.sin_addr.s_addr = inet_addr ("108.177.122.102");
+	
+	// //Fill in the IP Header
+	// iph->ihl = 5;
+	// iph->version = 4;
+	// iph->tos = 0;
+	// iph->tot_len = sizeof (struct iphdr) + sizeof (struct tcphdr) + strlen(data);
+	// iph->id = htonl (54321);	//Id of this packet
+	// iph->frag_off = 0;
+	// iph->ttl = 255;
+	// iph->protocol = IPPROTO_TCP;
+	// iph->check = 0;		//Set to 0 before calculating checksum
+	// iph->saddr = inet_addr ( source_ip );	//Spoof the source ip address
+	// iph->daddr = sin.sin_addr.s_addr;
+
+    // printf("datagram: %s\n", datagram);
+    // printf("data: %s\n", data);
+    
+
+    // //Send the packet
+    // if (sendto (s, datagram, iph->tot_len ,	0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
+    // {
+    //     perror("sendto failed");
+    // }
+    // //Data send successfully
+    // else
+    // {
+    //     printf ("Packet Send. Length : %d \n" , iph->tot_len);
+    // }
+    // // sleep for 1 seconds
+    // sleep(1);
+
+    // int bytes_recieved;
+
+    // unsigned char *buffer = (unsigned char *) malloc(65536); //to receive data
+    // memset(buffer,0,65536);
+    // struct sockaddr saddr;
+    // int saddr_len = sizeof (saddr);
+
+    // bytes_recieved = recvfrom(s,buffer,65536,0,&saddr,(socklen_t *)&saddr_len);
+
+    // // printf("&saddr: %s\n", &saddr);
+    // printf("&saddr_len: %d\n", saddr_len);
+
+    // printf ("response: %s\n", buffer);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
--- test.c
+++ test.c
@@ -1,7 +1,35 @@
-#include <stdio.h>    
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h> 
+#include <unistd.h> 
+#include <string.h>
+#include <ctype.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/tcp.h>	//Provides declarations for tcp header
+#include <netinet/ip.h>	//Provides declarations for ip header
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h> //For errno - the error number
+
+
 int main()
 { 
-    // Displays the string inside quotations
-    printf("C Programming");
+
+    int sendsock = socket (AF_INET, SOCK_RAW, IPPROTO_TCP);
+
+	int one = 1;
+	const int *val = &one;
+	
+	if (setsockopt (sendsock, IPPROTO_IP, IP_HDRINCL, val, sizeof (one)) < 0)
+	{
+		perror("Error setting IP_HDRINCL");
+		exit(0);
+	}
+
     return 0;
 }
