9c762105df5e601492192b57cc41b9426722e1b9
sking115422 <spencerd.king@gmail.com>
2021-11-13 07:22:31 +0000

    reading ICMP packets

--- tcp_traceroute.c
+++ tcp_traceroute.c
@@ -16,6 +16,10 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+#include <netinet/if_ether.h>
+#include <net/ethernet.h>
+#include <netinet/ether.h>
+
 
 
 int checkStringIsNumeric (char *str)
@@ -302,19 +306,60 @@ int main(int argc, char **argv)
 
             clock_t end = 0;
 
+
+            
+
+            unsigned char * buffer = (unsigned char *) malloc(65536);
+            memset(buffer, 0 ,65536);
             struct sockaddr saddr;
             int saddr_size = sizeof(saddr);
 
-            unsigned char * buf = (unsigned char *) malloc(65536);
-
-            int bytes_recieved;
-            if (bytes_recieved = recvfrom(recvsock_icmp, buf, 65536, 0, &saddr, &saddr_size) < 0)
+            int bytes_recieved = recvfrom(recvsock_icmp, buffer, 65536, 0, &saddr, &saddr_size);
+            if(bytes_recieved < 0)
             {
                 printf("*   ");
             }
             else
-            {
+            {   
                 end = clock();
+
+                struct ethhdr *eth = (struct ethhdr *)(buffer);
+                printf("\nEthernet Header\n");
+                printf("\t|-Source Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n",eth->h_source[0],eth->h_source[1],eth->h_source[2],eth->h_source[3],eth->h_source[4],eth->h_source[5]);
+                printf("\t|-Destination Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n",eth->h_dest[0],eth->h_dest[1],eth->h_dest[2],eth->h_dest[3],eth->h_dest[4],eth->h_dest[5]);
+                printf("\t|-Protocol : %d\n",eth->h_proto);
+
+                struct sockaddr_in source;
+                struct sockaddr_in dest;
+
+                unsigned short iphdrlen;
+                struct iphdr *ip = (struct iphdr*)(buffer + sizeof(struct ethhdr));
+                memset(&source, 0, sizeof(source));
+                source.sin_addr.s_addr = ip->saddr;
+                memset(&dest, 0, sizeof(dest));
+                dest.sin_addr.s_addr = ip->daddr;
+
+                printf("\nIP Header\n");
+                printf("\t|-Version : %d\n",(unsigned int)ip->version);
+                
+                printf("\t|-Internet Header Length : %d DWORDS or %d Bytes\n",(unsigned int)ip->ihl,((unsigned int)(ip->ihl))*4);
+                
+                printf("\t|-Type Of Service : %d\n",(unsigned int)ip->tos);
+                
+                printf("\t|-Total Length : %d Bytes\n",ntohs(ip->tot_len));
+                
+                printf("\t|-Identification : %d\n",ntohs(ip->id));
+                
+                printf("\t|-Time To Live : %d\n",(unsigned int)ip->ttl);
+                
+                printf("\t|-Protocol : %d\n",(unsigned int)ip->protocol);
+                
+                printf("\t|-Header Checksum : %d\n",ntohs(ip->check));
+                
+                printf("\t|-Source IP : %s\n", inet_ntoa(source.sin_addr));
+                
+                printf("\t|-Destination IP : %s\n",inet_ntoa(dest.sin_addr));
+
             }
 
             double time_spent;
